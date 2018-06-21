ee78bec2333d5407ad053b216840840443699137
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-06-21 13:20:33 -0700

    Revert "refactor: no need for separate Book component"
    This reverts commit 9074d51ec2932a012ab17b044fe2a8f7c162fa59.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,7 +1,6 @@
 import React from 'react'
+import Book from './Book'
 import './App.css'
-
-const BOOK_WIDTH = 128
 
 class BookShelf extends React.Component {
   render() {
@@ -12,22 +11,7 @@ class BookShelf extends React.Component {
           <ol className="books-grid">
             {this.props.books.map((book) => (
               <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover"><img width={BOOK_WIDTH} src={book.imageLinks.smallThumbnail} alt="" /></div>
-                    <div className="book-shelf-changer">
-                      <select value={book.shelf} onChange={(event) => this.props.onShelfChange(book, event.target.value)}>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors.join(', ')}</div>
-                </div>
+                <Book title={book.title} authors={book.authors} thumbnailUrl={book.imageLinks.smallThumbnail} />
               </li>
             ))}
           </ol>
