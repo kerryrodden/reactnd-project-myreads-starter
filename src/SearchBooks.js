3d1423ceaa31eb31d428c0f45e7cf829d8ffb479
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-06-21 13:17:36 -0700

    feat: add search (no results rendering yet)

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class SearchBooks extends React.Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+    BooksAPI.search(query).then((results) => this.setState({ results }))
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+
   render() {
     return (
       <div className="search-books">
@@ -18,8 +32,8 @@ class SearchBooks extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input type="text" placeholder="Search by title or author" />
-
+            <input type="text" placeholder="Search by title or author" value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
